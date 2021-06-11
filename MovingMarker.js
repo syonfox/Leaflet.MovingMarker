60613fe3d4c35d7499b0d68e9d35cbd15747dea5
kk <kanbang@163.com>
2021-06-11 17:34:52 +0800

    append commit

--- MovingMarker.js
+++ MovingMarker.js
@@ -1,9 +1,18 @@
-L.interpolatePosition = function(p1, p2, duration, t) {
-    var k = t/duration;
+L.interpolatePosition = function (p1, p2, duration, t) {
+    var k = t / duration;
     k = (k > 0) ? k : 0;
     k = (k > 1) ? 1 : k;
     return L.latLng(p1.lat + k * (p2.lat - p1.lat),
         p1.lng + k * (p2.lng - p1.lng));
+};
+
+L.getAngle = function (cx, cy, ex, ey) {
+    var dy = ey - cy;
+    var dx = ex - cx;
+    var theta = Math.atan2(dy, dx); // range (-PI, PI]
+    theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+    // if (theta < 0) theta = 360 + theta; // range [0, 360)
+    return theta;
 };
 
 L.Marker.MovingMarker = L.Marker.extend({
@@ -19,12 +28,23 @@ L.Marker.MovingMarker = L.Marker.extend({
     options: {
         autostart: false,
         loop: false,
+        rotate: false,
+        initialRotationAngle: 0,
+        rotationOrigin: "center",
     },
 
-    initialize: function (latlngs, durations, options) {
+    now: function () {
+        return Date.now();
+    },
+
+    initialize: function (latlngs, durations, options, cbmove, cbresetpos) {
+
+        this.cbmove = cbmove;
+        this.cbresetpos = cbresetpos;
+
         L.Marker.prototype.initialize.call(this, latlngs[0], options);
 
-        this._latlngs = latlngs.map(function(e, index) {
+        this._latlngs = latlngs.map(function (e, index) {
             return L.latLng(e);
         });
 
@@ -39,7 +59,6 @@ L.Marker.MovingMarker = L.Marker.extend({
 
         this._state = L.Marker.MovingMarker.notStartedState;
         this._startTime = 0;
-        this._startTimeStamp = 0;  // timestamp given by requestAnimFrame
         this._pauseStartTime = 0;
         this._animId = 0;
         this._animRequested = false;
@@ -47,23 +66,23 @@ L.Marker.MovingMarker = L.Marker.extend({
         this._stations = {};
     },
 
-    isRunning: function() {
+    isRunning: function () {
         return this._state === L.Marker.MovingMarker.runState;
     },
 
-    isEnded: function() {
+    isEnded: function () {
         return this._state === L.Marker.MovingMarker.endedState;
     },
 
-    isStarted: function() {
+    isStarted: function () {
         return this._state !== L.Marker.MovingMarker.notStartedState;
     },
 
-    isPaused: function() {
+    isPaused: function () {
         return this._state === L.Marker.MovingMarker.pausedState;
     },
 
-    start: function() {
+    start: function () {
         if (this.isRunning()) {
             return;
         }
@@ -72,13 +91,52 @@ L.Marker.MovingMarker = L.Marker.extend({
             this.resume();
         } else {
             this._loadLine(0);
+            // callback: reset
+            if (this.cbresetpos) {
+                this.cbresetpos();
+            }
             this._startAnimation();
             this.fire('start');
         }
     },
 
-    resume: function() {
-        if (! this.isPaused()) {
+    setcurpos: function (val) {
+        var totaltime = 0;
+        for (const it of this._durations) {
+            totaltime += it;
+        }
+        var passtime = totaltime * val;
+
+        this._loadLine(0);
+
+        var now = this.now();
+        this._startTime = now - passtime;
+        this._pauseStartTime = now;
+
+        // callback: reset
+        if (this.cbresetpos) {
+            this.cbresetpos();
+        }
+
+        // callback: move to start
+        if (this.cbmove && this._latlngs.length) {
+            this.cbmove(this._latlngs[0]);
+        }
+
+        // update line & move position
+        this._animate(true);
+
+        if (this._state == L.Marker.MovingMarker.endedState) {
+            this._state = L.Marker.MovingMarker.runState;
+            this._animId = L.Util.requestAnimFrame(function (timestamp) {
+                this._animate();
+            }, this, true);
+            this._animRequested = true;
+        }
+    },
+
+    resume: function () {
+        if (!this.isPaused()) {
             return;
         }
         // update the current line
@@ -87,40 +145,42 @@ L.Marker.MovingMarker = L.Marker.extend({
         this._startAnimation();
     },
 
-    pause: function() {
-        if (! this.isRunning()) {
+    pause: function () {
+        if (!this.isRunning()) {
             return;
         }
 
-        this._pauseStartTime = Date.now();
+        this._pauseStartTime = this.now();
         this._state = L.Marker.MovingMarker.pausedState;
         this._stopAnimation();
         this._updatePosition();
     },
 
-    stop: function(elapsedTime) {
+    stop: function (elapsedTime) {
         if (this.isEnded()) {
             return;
         }
 
         this._stopAnimation();
 
-        if (typeof(elapsedTime) === 'undefined') {
+        if (typeof (elapsedTime) === 'undefined') {
             // user call
             elapsedTime = 0;
             this._updatePosition();
         }
 
         this._state = L.Marker.MovingMarker.endedState;
-        this.fire('end', {elapsedTime: elapsedTime});
+        this.fire('end', {
+            elapsedTime: elapsedTime
+        });
     },
 
-    addLatLng: function(latlng, duration) {
+    addLatLng: function (latlng, duration) {
         this._latlngs.push(L.latLng(latlng));
         this._durations.push(duration);
     },
 
-    moveTo: function(latlng, duration) {
+    moveTo: function (latlng, duration) {
         this._stopAnimation();
         this._latlngs = [this.getLatLng(), L.latLng(latlng)];
         this._durations = [duration];
@@ -129,7 +189,7 @@ L.Marker.MovingMarker = L.Marker.extend({
         this.options.loop = false;
     },
 
-    addStation: function(pointIndex, duration) {
+    addStation: function (pointIndex, duration) {
         if (pointIndex > this._latlngs.length - 2 || pointIndex < 1) {
             return;
         }
@@ -139,7 +199,7 @@ L.Marker.MovingMarker = L.Marker.extend({
     onAdd: function (map) {
         L.Marker.prototype.onAdd.call(this, map);
 
-        if (this.options.autostart && (! this.isStarted())) {
+        if (this.options.autostart && (!this.isStarted())) {
             this.start();
             return;
         }
@@ -149,7 +209,7 @@ L.Marker.MovingMarker = L.Marker.extend({
         }
     },
 
-    onRemove: function(map) {
+    onRemove: function (map) {
         L.Marker.prototype.onRemove.call(this, map);
         this._stopAnimation();
     },
@@ -177,41 +237,62 @@ L.Marker.MovingMarker = L.Marker.extend({
         return durations;
     },
 
-    _startAnimation: function() {
+    _startAnimation: function () {
         this._state = L.Marker.MovingMarker.runState;
-        this._animId = L.Util.requestAnimFrame(function(timestamp) {
-            this._startTime = Date.now();
-            this._startTimeStamp = timestamp;
-            this._animate(timestamp);
+        this._animId = L.Util.requestAnimFrame(function (timestamp) {
+            this._startTime = this.now();
+            this._animate();
         }, this, true);
         this._animRequested = true;
     },
 
-    _resumeAnimation: function() {
-        if (! this._animRequested) {
+    _resumeAnimation: function () {
+        if (!this._animRequested) {
             this._animRequested = true;
-            this._animId = L.Util.requestAnimFrame(function(timestamp) {
-                this._animate(timestamp);
+            this._animId = L.Util.requestAnimFrame(function (timestamp) {
+                this._animate();
             }, this, true);
         }
     },
 
-    _stopAnimation: function() {
+    _stopAnimation: function () {
         if (this._animRequested) {
             L.Util.cancelAnimFrame(this._animId);
             this._animRequested = false;
         }
     },
 
-    _updatePosition: function() {
-        var elapsedTime = Date.now() - this._startTime;
-        this._animate(this._startTimeStamp + elapsedTime, true);
+    _updatePosition: function () {
+        this._animate(true);
     },
 
-    _loadLine: function(index) {
+    _updateRotation: function () {
+        if (this._rotationAngle) {
+            this._icon.style[
+                L.DomUtil.TRANSFORM + "Origin"
+            ] = this.options.rotationOrigin;
+
+            this._icon.style[L.DomUtil.TRANSFORM] +=
+                " rotateZ(" +
+                (this.options.initialRotationAngle + this._rotationAngle) +
+                "deg)";
+        }
+    },
+
+    _loadLine: function (index) {
         this._currentIndex = index;
         this._currentDuration = this._durations[index];
         this._currentLine = this._latlngs.slice(index, index + 2);
+
+        if (this.options.rotate) {
+            // set direction
+            this._rotationAngle = L.getAngle(
+                this._currentLine[0].lat,
+                this._currentLine[0].lng,
+                this._currentLine[1].lat,
+                this._currentLine[1].lng
+            );
+        }
     },
 
     /**
@@ -220,9 +301,9 @@ L.Marker.MovingMarker = L.Marker.extend({
      * @return {Number} elapsed time on the current line or null if
      * we reached the end or marker is at a station
      */
-    _updateLine: function(timestamp) {
+    _updateLine: function (timestamp) {
         // time elapsed since the last latlng
-        var elapsedTime = timestamp - this._startTimeStamp;
+        var elapsedTime = timestamp - this._startTime;
 
         // not enough time to update the line
         if (elapsedTime <= this._currentDuration) {
@@ -241,10 +322,15 @@ L.Marker.MovingMarker = L.Marker.extend({
             // test if there is a station at the end of the line
             if (stationDuration !== undefined) {
                 if (elapsedTime < stationDuration) {
-                    this.setLatLng(this._latlngs[lineIndex + 1]);
+                    this._setLatLngWithCb(this._latlngs[lineIndex + 1]);
                     return null;
                 }
                 elapsedTime -= stationDuration;
+            }
+
+            // key point
+            if (this.cbmove) {
+                this.cbmove(this._latlngs[lineIndex + 1])
             }
 
             lineIndex++;
@@ -254,11 +340,13 @@ L.Marker.MovingMarker = L.Marker.extend({
 
                 if (this.options.loop) {
                     lineIndex = 0;
-                    this.fire('loop', {elapsedTime: elapsedTime});
+                    this.fire('loop', {
+                        elapsedTime: elapsedTime
+                    });
                 } else {
                     // place the marker at the end, else it would be at
                     // the last position
-                    this.setLatLng(this._latlngs[this._latlngs.length - 1]);
+                    this._setLatLngWithCb(this._latlngs[this._latlngs.length - 1]);
                     this.stop(elapsedTime);
                     return null;
                 }
@@ -267,16 +355,25 @@ L.Marker.MovingMarker = L.Marker.extend({
         }
 
         this._loadLine(lineIndex);
-        this._startTimeStamp = timestamp - elapsedTime;
-        this._startTime = Date.now() - elapsedTime;
+        this._startTime = this.now() - elapsedTime;
         return elapsedTime;
     },
 
-    _animate: function(timestamp, noRequestAnim) {
+    _setLatLngWithCb: function (p) {
+        this.setLatLng(p);
+        if (this.options.rotate) {
+            this._updateRotation();
+        }
+        if (this.cbmove) {
+            this.cbmove(p)
+        }
+    },
+
+    _animate: function (noRequestAnim) {
         this._animRequested = false;
 
         // find the next line and compute the new elapsedTime
-        var elapsedTime = this._updateLine(timestamp);
+        var elapsedTime = this._updateLine(this.now());
 
         if (this.isEnded()) {
             // no need to animate
@@ -284,21 +381,26 @@ L.Marker.MovingMarker = L.Marker.extend({
         }
 
         if (elapsedTime != null) {
-             // compute the position
+            // compute the position
             var p = L.interpolatePosition(this._currentLine[0],
                 this._currentLine[1],
                 this._currentDuration,
                 elapsedTime);
-            this.setLatLng(p);
+            this._setLatLngWithCb(p);
         }
 
-        if (! noRequestAnim) {
-            this._animId = L.Util.requestAnimFrame(this._animate, this, false);
-            this._animRequested = true;
+        if (noRequestAnim) {
+            return;
         }
+
+        this._animId = L.Util.requestAnimFrame(() => {
+            this._animate();
+        }, this, false);
+
+        this._animRequested = true;
     }
 });
 
-L.Marker.movingMarker = function (latlngs, duration, options) {
-    return new L.Marker.MovingMarker(latlngs, duration, options);
+L.Marker.movingMarker = function (latlngs, duration, options, cbmove, cbresetpos) {
+    return new L.Marker.MovingMarker(latlngs, duration, options, cbmove, cbresetpos);
 };
